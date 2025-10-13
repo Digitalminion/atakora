f95d478d14e5041255a8295d96cae4b5c723cfac
Austin Leahy <leahy@apache.org>
2025-10-13 16:42:54 -0500

    new microsoft schema

--- packages/lib/src/generated/types/index.ts
+++ packages/lib/src/generated/types/index.ts
@@ -10,12 +10,25 @@
  * import * as Network from './Microsoft.Network.NRP';
  * import * as Storage from './Microsoft.Storage';
  * import * as KeyVault from './Microsoft.KeyVault';
+ * import * as DBforMariaDB from './Microsoft.DBforMariaDB';
+ * import * as DBforMySQL from './Microsoft.DBforMySQL';
  * ```
  *
  * @packageDocumentation
  */
 
 // Export namespace modules to avoid naming conflicts
-export * as Network from './Microsoft.Network.NRP';
-export * as Storage from './Microsoft.Storage';
+export * as AAD from './Microsoft.AAD';
+export * as ApiManagement from './Microsoft.ApiManagement';
+export * as Authorization from './Microsoft.Authorization';
+export * as AzureActiveDirectory from './Microsoft.AzureActiveDirectory';
+export * as DBforMariaDB from './Microsoft.DBforMariaDB';
+export * as DBforMySQL from './Microsoft.DBforMySQL';
+export * as DocumentDB from './Microsoft.DocumentDB';
 export * as KeyVault from './Microsoft.KeyVault';
+export * as Network from './Microsoft.Network.NRP';
+export * as OperationalInsights from './Microsoft.OperationalInsights';
+export * as Purview from './Microsoft.Purview';
+export * as Sql from './Microsoft.Sql';
+export * as Storage from './Microsoft.Storage';
+export * as Web from './Microsoft.Web';
--- packages/lib/src/generated/validation/index.ts
+++ packages/lib/src/generated/validation/index.ts
@@ -11,11 +11,38 @@
 // Re-export validation types
 export type { ValidationError, ValidationResult } from './Microsoft.Network.NRP.validators';
 
-// Microsoft.Network validators (2024-07-01)
-export * from './Microsoft.Network.NRP.validators';
+// Microsoft.AAD validators (2025-06-01)
+export * from './Microsoft.AAD.validators';
 
-// Microsoft.Storage validators (2025-01-01)
-export * from './Microsoft.Storage.validators';
+// Microsoft.ApiManagement validators (2024-10-01-preview)
+export * from './Microsoft.ApiManagement.validators';
+
+// Microsoft.Authorization validators (2022-04-01)
+export * from './Microsoft.Authorization.validators';
+
+// Microsoft.AzureActiveDirectory validators (2023-05-17-preview)
+export * from './Microsoft.AzureActiveDirectory.validators';
+
+// Microsoft.DBforMariaDB validators (2018-06-01)
+export * from './Microsoft.DBforMariaDB.validators';
+
+// Microsoft.DBforMySQL validators (2024-12-01-preview)
+export * from './Microsoft.DBforMySQL.validators';
+
+// Microsoft.DocumentDB validators (2024-08-15)
+export * from './Microsoft.DocumentDB.validators';
 
 // Microsoft.KeyVault validators (2024-12-01-preview)
 export * from './Microsoft.KeyVault.validators';
+
+// Microsoft.Network validators (2024-07-01)
+export * from './Microsoft.Network.NRP.validators';
+
+// Microsoft.OperationalInsights validators (2023-09-01)
+export * from './Microsoft.OperationalInsights.validators';
+
+// Microsoft.Purview validators (2024-04-01-preview)
+export * from './Microsoft.Purview.validators';
+
+// Microsoft.Storage validators (2025-01-01)
+export * from './Microsoft.Storage.validators';
